276b40b326aed41dce917f5af8a3d0aefbc50f27
anthonytranDev <anthonytran.dev@gmail.com>
2020-09-04 18:40:04 +0100

    feat: get requested message data to display on screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
+
+const params = '/messages';
+
+const url = `http://localhost:3001${params}`;
 
 function App() {
-  return <div></div>;
+  const [messages, setMessages] = useState([]);
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => setMessages(data));
+
+  return <div>{JSON.stringify(messages)}</div>;
 }
 
 export default App;
